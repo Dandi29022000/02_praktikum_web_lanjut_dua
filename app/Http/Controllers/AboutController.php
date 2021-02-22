e311f59d481c300e8e9e34592eeafababad570cf
= <=>
2021-02-22 22:20:12 +0700

    Praktikum 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function about(){
+        echo "Nama : Dandi Agung Setiawan <br> NIM : 1941720009";
+    }
+}
